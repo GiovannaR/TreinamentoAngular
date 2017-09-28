0507d3d3eed698b6deb9528e19ff6f5bb2a2fa8c
GiovannaRiqueti <giovannariqueti@gmail.com>
2017-09-28 13:56:56 -0300

    9.13 finalizada

---
+++ src/app/app.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  ultimoId = 0;
+  nome = '';
+  adicionado = false;
+  funcionarios = [];
+
+  adicionar(){
+    this.adicionado = true;
+    this.funcionarios.push({
+      id: ++this.ultimoId,
+      nome: this.nome
+    });
+  }
+
+  alterarNome (event: any){
+    
+  }
+
+
+
+}
+
---
+++ src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { HelloComponent } from './Hello/hello.component';
+import { BemVindoComponent } from './bem-vindo/bem-vindo.component'
+@NgModule({
+  declarations: [
+    AppComponent,
+    HelloComponent,
+    BemVindoComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
